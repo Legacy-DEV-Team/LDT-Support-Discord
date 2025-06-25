441dab3f2862e03209bc1039a8aef7686df37aed
Angel Knutsen Aune <djangel@live.no>
2025-06-25 21:42:35 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,24 +7,25 @@ const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) { 
-const command = require(`./commands/${file}`); 
-commands.push(command.data.toJSON());
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '10' }).setToken(config.token);
 
-(async () => { 
-try { 
-console.log(`🚀 Logging ${commands.length} slash command(s)...`); 
+(async () => {
+  try {
+    console.log(`🚀 Registering ${commands.length} slash command(s) to guild ${config.guildId}...`);
 
-await rest.put( 
-Routes.applicationGuildCommands(config.clientId, config.guildId), 
-{ body: commands }, 
-); 
+    await rest.put(
+      Routes.applicationGuildCommands(config.clientId, config.guildId),
+      { body: commands }
+    );
 
-console.log(' ✅ Slash command(s) registered.'); 
-} catch (error) { 
-console.error('❌ Could not register commands:', error); 
-}
+    console.log('\x1b[32m%s\x1b[0m', '✅ Slash command(s) registered successfully.');
+  } catch (error) {
+    console.error('\x1b[31m%s\x1b[0m', '❌ Failed to register commands:');
+    console.error(error);
+  }
 })();
